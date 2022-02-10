3958a82e9c9ead37eebadf340f949e57e25fc928
kotovi4 <kotovi4.m@gmail.com>
2022-02-11 00:20:34 +0300

    нарезает графику

--- gulpfile.js
+++ gulpfile.js
@@ -66,7 +66,7 @@ gulp.task("webp", function () {
 });
 
 gulp.task("sprite", function () {
-  return gulp.src("source/img/{icon-*,htmlacademy*}.svg")
+  return gulp.src("source/img/*.svg")
     .pipe(svgstore({inlineSvg: true}))
     .pipe(rename("sprite_auto.svg"))
     .pipe(gulp.dest("build/img"));
@@ -83,7 +83,7 @@ gulp.task("html", function () {
 gulp.task("copy", function () {
   return gulp.src([
     "source/fonts/**/*.{woff,woff2}",
-    "source/img/**",
+    "source/img/**.jpg",
     "source/js/**",
     "source//*.ico"
     ], {
