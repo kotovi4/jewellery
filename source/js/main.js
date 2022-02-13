8e4910e55034964ddb4cdffa035dfc345aa5a6db
kotovi4 <kotovi4.m@gmail.com>
2022-02-13 16:10:54 +0300

    добавляет раздел new на таблет и мобиле

---
+++ source/js/main.js
@@ -0,0 +1,8 @@
+// //Slider
+// const newCard = document.querySelectorAll('.new__cards li');
+// const arrowLeft = document.querySelector('.new__slider-button--left');
+// const arrowRight = document.querySelector('.new__slider-button--right');
+
+// arrowLeft.addEventListener('click',function() {
+//   newCard.style.left =
+// })
