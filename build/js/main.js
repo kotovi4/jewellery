7cbc963d32ba663eaa3ea0c786426b40812f4b30
kotovi4 <kotovi4.m@gmail.com>
2022-02-21 18:32:06 +0300

    добавляет открытие и закрытие меню и фильтра

--- build/js/main.js
+++ build/js/main.js
@@ -1,3 +1,20 @@
+//открытие и закрытие меню по кнопке крестика
+const pageBody = document.querySelector('.page-body')
+const menuOpenButton = document.querySelector('.page-header__toggle');
+const pageHeader = document.querySelector('.page-header');
+
+pageBody.classList.remove('page-body--nojs');
+pageHeader.classList.remove('page-header--opened');
+menuOpenButton.addEventListener('click', () => pageHeader.classList.toggle('page-header--opened'));
+
+//открыие фильтра по кнопке
+const filterOpenButton = document.querySelector('.filter__button--open');
+const filterCloseButton = document.querySelector('.filter__button--close');
+const filter = document.querySelector('.filter');
+
+filterOpenButton.addEventListener('click', () => filter.classList.add('filter--opened'));
+filterCloseButton.addEventListener('click', () => filter.classList.remove('filter--opened'));
+
 // //Slider
 // const newCard = document.querySelectorAll('.new__cards li');
 // const arrowLeft = document.querySelector('.new__slider-button--left');
